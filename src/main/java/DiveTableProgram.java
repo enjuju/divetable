252cc40cc10dd594099ec4284dcd4b7363289ea9
Patricia Kamlley <pkamlley@gmail.com>
2022-11-03 20:46:04 -0700

    Added depth and bottom time rounder, but only for depth A. Main program accepts input

--- src/main/java/DiveTableProgram.java
+++ src/main/java/DiveTableProgram.java
@@ -1,9 +1,25 @@
 import com.techelevator.DiveTable;
 
+import java.util.Scanner;
+
 public class DiveTableProgram {
     public static void main(String[] args) {
-        DiveTable diveTable = new DiveTable(35, 10, 60);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What was your depth? ");
+        int depth = input.nextInt();
+        input.nextLine();
+
+        System.out.print("What was your bottom time? ");
+        int bottomTime = input.nextInt();
+        input.nextLine();
+
+        System.out.print("What was your surface interval? ");
+        int surfaceInterval = input.nextInt();
+        input.nextLine();
+
+        DiveTable diveTable = new DiveTable(depth, bottomTime, surfaceInterval);
         char pressureGroup = diveTable.calculatePressureGroup();
-        System.out.println(pressureGroup);
+        System.out.println("Your pressure group is now: " + pressureGroup);
     }
 }
--- src/main/java/com/techelevator/DiveTable.java
+++ src/main/java/com/techelevator/DiveTable.java
@@ -11,6 +11,16 @@ public class DiveTable {
     private char pressureGroup;
     private int surfaceInterval;
     private char pressureGroupAfterSI;
+    private String dKey;
+    private String tKey;
+
+    public String getTKey() {
+        return tKey;
+    }
+
+    public String getDKey() {
+        return dKey;
+    }
 
     public char getPressureGroup() {
         return pressureGroup;
@@ -56,19 +66,134 @@ public class DiveTable {
 
     public Map<String, Character> map = Map.of(
             "35,10", 'A',
-            "35,19", 'B');
+            "35,19", 'B',
+            "35,25",'C',
+            "35,29", 'D',
+            "35,32", 'E');
 
     public char calculatePressureGroup(){
-        String dHash = "";
-        String tHash = "";
+        depthRoundUp();
+        bottomTimeRoundUp();
+        String dTKey = dKey + "," + tKey;
+        return map.get(dTKey);
+    }
+    public String depthRoundUp(){
         if (depth <= 35){
-            dHash = "35";
+            dKey = "35";
         }
-        if (bottomTime <=10){
-            tHash = "10";
+        else if (depth <= 40){
+            dKey = "40";
         }
-        String DTHash = dHash +","+tHash;
-        return map.get(DTHash);
+        else if (depth <= 50){
+            dKey = "50";
+        }
+        else if (depth <= 60){
+            dKey = "60";
+        }
+        else if (depth <= 70){
+            dKey = "70";
+        }
+        else if (depth <= 80){
+            dKey = "80";
+        }
+        else if (depth <= 90){
+            dKey = "90";
+        }
+        else if (depth <= 100){
+            dKey = "100";
+        }
+        else if (depth <= 110){
+            dKey = "110";
+        }
+        else if (depth <= 120){
+            dKey = "120";
+        }
+        else if (depth <= 130){
+            dKey = "130";
+        }
+        else if (depth <= 140){
+            dKey = "140";
+        }
+        return dKey;
+    }
+    public String bottomTimeRoundUp(){
+        if (getDKey() == "35"){
+            if (bottomTime <=10){
+                tKey = "10";
+            }
+            else if (bottomTime <=19){
+                tKey = "19";
+            }
+            else if (bottomTime <=25){
+                tKey = "25";
+            }
+            else if (bottomTime <=29){
+                tKey = "29";
+            }
+            else if (bottomTime <=32){
+                tKey = "32";
+            }
+            else if (bottomTime <=36){
+                tKey = "36";
+            }
+            else if (bottomTime <=40){
+                tKey = "40";
+            }
+            else if (bottomTime <=44){
+                tKey = "44";
+            }
+            else if (bottomTime <=48){
+                tKey = "48";
+            }
+            else if (bottomTime <=52){
+                tKey = "52";
+            }
+            else if (bottomTime <=57){
+                tKey = "57";
+            }
+            else if (bottomTime <=62){
+                tKey = "62";
+            }
+            else if (bottomTime <=67){
+                tKey = "67";
+            }
+            else if (bottomTime <=73){
+                tKey = "73";
+            }
+            else if (bottomTime <=85) {
+                tKey = "85";}
+            else if (bottomTime <= 92) {
+                tKey = "92";
+            }
+            else if (bottomTime <= 100) {
+                tKey = "100";
+            }
+            else if (bottomTime <= 108) {
+                tKey = "108";
+            }
+            else if (bottomTime <= 117) {
+                tKey = "117";
+            }
+            else if (bottomTime <= 127) {
+                tKey = "127";
+            }
+            else if (bottomTime <= 139) {
+                tKey = "139";
+            }
+            else if (bottomTime <= 152) {
+                tKey = "152";
+            }
+            else if (bottomTime <= 168) {
+                tKey = "168";
+            }
+            else if (bottomTime <= 188) {
+                tKey = "188";
+            }
+            else if (bottomTime <= 205) {
+                tKey = "205";
+            }
+        }
+        return tKey;
     }
 
 }
